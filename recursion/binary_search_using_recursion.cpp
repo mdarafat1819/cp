25a88350a3f8dc26962a3907b2d22a6d6a8e2e0a
Yeasin Arafat <mdarafat1819@gmail.com>
2021-10-24 17:01:54 +0600

    reverse linked list using recursion

--- recursion/binary_search_using_recursion.cpp
+++ recursion/binary_search_using_recursion.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int binary_searc(int arr[], int left, int right, int value) 
+int binary_search(int arr[], int left, int right, int value) 
 {
     if(left > right){
         return -1;
@@ -11,15 +11,15 @@ int binary_searc(int arr[], int left, int right, int value)
     int mid = (left + right) / 2;
     if(arr[mid] == value) return mid;
 
-    if(arr[mid] > value) return binary_searc(arr,  mid + 1, right, value);
-    return binary_searc(arr, left, mid-1, value);
+    if(arr[mid] > value) return binary_search(arr,  mid + 1, right, value);
+    return binary_search(arr, left, mid-1, value);
 }
 
 int main()
 {
     int arr[] = {5, 10, 12, 17, 18, 19, 25, 23};
 
-    cout<<binary_searc(arr, 0, 7, 10)<<endl;
+    cout<<binary_search(arr, 0, 7, 10)<<endl;
 
     return 0;
 }
